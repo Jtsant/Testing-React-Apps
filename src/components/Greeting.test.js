ea9f3b69b7e3778766a21f1697d497fed62cb40b
Jtsant <jtsant92@hotmail.com>
2022-09-27 14:58:18 +0300

    Creating the first test

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,3 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import Greeting from './Greeting';
+
 test('renders Hello World as a text', () => {
-    
+    // Arrrange
+    render(<Greeting />);
+
+    // Act
+    // ... nothing
+
+    // Assert
+    const helloWorldElement = screen.getByText('Hello World');
+    expect(helloWorldElement).toBeInTheDocument();
 });
