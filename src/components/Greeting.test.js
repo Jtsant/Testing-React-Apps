efe868252ab62b321704a5304927ae5a388703a0
Jtsant <jtsant92@hotmail.com>
2022-09-28 16:04:39 +0300

    User Interaction

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -13,4 +13,11 @@ describe("Greeting component", () => {
     const helloWorldElement = screen.getByText("Hello World");
     expect(helloWorldElement).toBeInTheDocument();
   });
+
+  test("renders good to see you if the button was NOT clicked", () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText("good to see you", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
 });
