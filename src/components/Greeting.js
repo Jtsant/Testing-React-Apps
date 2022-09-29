6949ea9a2768e849b1528ca3657c271cf8796653
Jtsant <jtsant92@hotmail.com>
2022-09-29 15:26:59 +0300

    User Interaction

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,19 +1,22 @@
-import { useState } from 'react';
+import { useState } from "react";
+
+import Output from "./Output";
 
 const Greeting = () => {
-    const [changedText, setChangedText] = useState(false);
+  const [changedText, setChangedText] = useState(false);
 
-    const changeTextHandler = () => {
-        setChangedText(true);
-    };
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
 
-    return <div>
-        <h2>Hello World</h2>
-        {!changedText && <p>It's good to see you!</p>}
-        {changedText && <p>Changed!</p>}
-        <button onClick={changeTextHandler}>Change Text!</button>
+  return (
+    <div>
+      <h2>Hello World!</h2>
+      {!changedText && <Output>It's good to see you!</Output>}
+      {changedText && <Output>Changed!</Output>}
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
+  );
 };
-
 
 export default Greeting;
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -3,19 +3,19 @@ import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
-  test("renders Hello World as a text", () => {
-    // Arrrange
+  test('renders "Hello World" as a text', () => {
+    // Arrange
     render(<Greeting />);
 
     // Act
     // ... nothing
 
     // Assert
-    const helloWorldElement = screen.getByText("Hello World");
+    const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
   });
 
-  test('renders "good to see you" if the button was NOT clicked', () => {
+  test('renders "good to see" you if the button was NOT clicked', () => {
     render(<Greeting />);
 
     const outputElement = screen.getByText("good to see you", { exact: false });
@@ -44,7 +44,9 @@ describe("Greeting component", () => {
     userEvent.click(buttonElement);
 
     // Assert
-    const outputElement = screen.queryByText("good to see you", { exact: false });
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
     expect(outputElement).toBeNull();
   });
 });
